beb167ab4b1a4154aa49d1b00edf44e77de680e7
Rico Sonntag <mail@ricosonntag.de>
2025-09-30 16:06:39 +0200

    BUGFIX: restore cluster consolidation alias (#83)

--- src/Service/Clusterer/ClusterConsolidationService.php
+++ src/Service/Clusterer/ClusterConsolidationService.php
@@ -11,11 +11,27 @@ declare(strict_types=1);
 
 namespace MagicSunday\Memories\Service\Clusterer;
 
+use MagicSunday\Memories\Clusterer\ClusterDraft;
+use MagicSunday\Memories\Service\Clusterer\Contract\ClusterConsolidatorInterface;
 use MagicSunday\Memories\Service\Clusterer\Pipeline\PipelineClusterConsolidator;
 
 /**
  * @deprecated Use PipelineClusterConsolidator with ClusterConsolidatorInterface instead.
  */
-final class ClusterConsolidationService extends PipelineClusterConsolidator
+final class ClusterConsolidationService implements ClusterConsolidatorInterface
 {
+    public function __construct(private readonly PipelineClusterConsolidator $consolidator)
+    {
+    }
+
+    /**
+     * @param list<ClusterDraft>                                     $drafts
+     * @param callable(int $done, int $max, string $stage):void|null $progress
+     *
+     * @return list<ClusterDraft>
+     */
+    public function consolidate(array $drafts, ?callable $progress = null): array
+    {
+        return $this->consolidator->consolidate($drafts, $progress);
+    }
 }
