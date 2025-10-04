b722b5291e84983c1df824c9a755fd53236e853e
Rico Sonntag <mail@ricosonntag.de>
2025-10-04 16:49:06 +0200

    Ensure pHash prefix setter truncates values

---
+++ test/Unit/Entity/MediaPhashPrefixTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * This file is part of the package magicsunday/photo-memories.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE file that was distributed with this source code.
+ */
+
+declare(strict_types=1);
+
+namespace MagicSunday\Memories\Test\Unit\Entity;
+
+use MagicSunday\Memories\Test\TestCase;
+use PHPUnit\Framework\Attributes\Test;
+
+final class MediaPhashPrefixTest extends TestCase
+{
+    #[Test]
+    public function truncatesPhashPrefixToSixteenCharacters(): void
+    {
+        $media = $this->makeMedia(
+            id: 201,
+            path: '/library/phash-prefix.jpg',
+        );
+
+        $media->setPhashPrefix('0123456789abcdefcafebabe');
+
+        self::assertSame('0123456789abcdef', $media->getPhashPrefix());
+    }
+}
