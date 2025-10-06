b06fcca408b1be87c965efa84efabc9c78b50255
Rico Sonntag <mail@ricosonntag.de>
2025-10-06 23:58:58 +0200

    TEST: cover persisted cluster parameters

--- test/Unit/Support/ClusterEntityToDraftMapperTest.php
+++ test/Unit/Support/ClusterEntityToDraftMapperTest.php
@@ -54,4 +54,51 @@ final class ClusterEntityToDraftMapperTest extends TestCase
 
         self::assertSame('custom_group', $drafts[0]->getParams()['group']);
     }
+
+    #[Test]
+    public function mapManyPreservesExtendedParameters(): void
+    {
+        $movement = [
+            'segment_count'      => 5,
+            'fast_segment_ratio' => 0.4,
+            'avg_speed_mps'      => 8.1,
+        ];
+
+        $params = [
+            'score'                => 0.95,
+            'group'                => 'travel_and_places',
+            'quality_avg'          => 0.82,
+            'quality_resolution'   => 0.91,
+            'people'               => 0.5,
+            'people_count'         => 6,
+            'people_unique'        => 3,
+            'people_coverage'      => 0.75,
+            'people_face_coverage' => 0.5,
+            'movement'             => $movement,
+        ];
+
+        $entity = new Cluster(
+            algorithm: 'vacation',
+            params: $params,
+            centroid: ['lat' => 1.0, 'lon' => 2.0],
+            members: [3, 1, 2],
+        );
+
+        $mapper = new ClusterEntityToDraftMapper(['vacation' => 'travel_and_places']);
+
+        $drafts = $mapper->mapMany([$entity]);
+
+        self::assertCount(1, $drafts);
+
+        $draftParams = $drafts[0]->getParams();
+
+        self::assertSame(0.82, $draftParams['quality_avg']);
+        self::assertSame(0.91, $draftParams['quality_resolution']);
+        self::assertSame(0.5, $draftParams['people']);
+        self::assertSame(6, $draftParams['people_count']);
+        self::assertSame(3, $draftParams['people_unique']);
+        self::assertSame(0.75, $draftParams['people_coverage']);
+        self::assertSame(0.5, $draftParams['people_face_coverage']);
+        self::assertSame($movement, $draftParams['movement']);
+    }
 }
