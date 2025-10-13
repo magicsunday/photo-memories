4c8261958d068bc6656706f1687119337e182a90
Rico Sonntag <mail@ricosonntag.de>
2025-10-13 16:45:20 +0200

    fix(indexing): skip near-duplicate stage for unmanaged media

--- test/Unit/Service/Indexing/Stage/NearDuplicateStageTest.php
+++ test/Unit/Service/Indexing/Stage/NearDuplicateStageTest.php
@@ -11,6 +11,7 @@ declare(strict_types=1);
 
 namespace MagicSunday\Memories\Test\Unit\Service\Indexing\Stage;
 
+use Doctrine\ORM\EntityManagerInterface;
 use MagicSunday\Memories\Entity\MediaDuplicate;
 use MagicSunday\Memories\Repository\MediaDuplicateRepository;
 use MagicSunday\Memories\Repository\MediaRepository;
@@ -45,7 +46,13 @@ final class NearDuplicateStageTest extends TestCase
             ->with($primary, $secondary, 4)
             ->willReturn(new MediaDuplicate($primary, $secondary, 4));
 
-        $stage = new NearDuplicateStage($mediaRepository, $duplicateRepository);
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->expects(self::once())
+            ->method('contains')
+            ->with($primary)
+            ->willReturn(true);
+
+        $stage = new NearDuplicateStage($entityManager, $mediaRepository, $duplicateRepository);
 
         $context = MediaIngestionContext::create(
             $primary->getPath(),
@@ -79,7 +86,13 @@ final class NearDuplicateStageTest extends TestCase
         $duplicateRepository = $this->createMock(MediaDuplicateRepository::class);
         $duplicateRepository->expects(self::never())->method('recordDistance');
 
-        $stage = new NearDuplicateStage($mediaRepository, $duplicateRepository);
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->expects(self::once())
+            ->method('contains')
+            ->with($primary)
+            ->willReturn(true);
+
+        $stage = new NearDuplicateStage($entityManager, $mediaRepository, $duplicateRepository);
 
         $context = MediaIngestionContext::create(
             $primary->getPath(),
@@ -106,7 +119,47 @@ final class NearDuplicateStageTest extends TestCase
         $duplicateRepository = $this->createMock(MediaDuplicateRepository::class);
         $duplicateRepository->expects(self::never())->method('recordDistance');
 
-        $stage = new NearDuplicateStage($mediaRepository, $duplicateRepository);
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->expects(self::once())
+            ->method('contains')
+            ->with($primary)
+            ->willReturn(true);
+
+        $stage = new NearDuplicateStage($entityManager, $mediaRepository, $duplicateRepository);
+
+        $context = MediaIngestionContext::create(
+            $primary->getPath(),
+            false,
+            false,
+            false,
+            false,
+            new BufferedOutput()
+        )->withMedia($primary);
+
+        $result = $stage->process($context);
+
+        self::assertSame($primary, $result->getMedia());
+    }
+
+    #[Test]
+    public function processSkipsUnmanagedMedia(): void
+    {
+        $primary = $this->makeMedia(40, '/library/unmanaged.jpg');
+        $primary->setPhash('cccccccccccccccccccccccccccccccc');
+
+        $mediaRepository = $this->createMock(MediaRepository::class);
+        $mediaRepository->expects(self::never())->method('findNearestByPhash');
+
+        $duplicateRepository = $this->createMock(MediaDuplicateRepository::class);
+        $duplicateRepository->expects(self::never())->method('recordDistance');
+
+        $entityManager = $this->createMock(EntityManagerInterface::class);
+        $entityManager->expects(self::once())
+            ->method('contains')
+            ->with($primary)
+            ->willReturn(false);
+
+        $stage = new NearDuplicateStage($entityManager, $mediaRepository, $duplicateRepository);
 
         $context = MediaIngestionContext::create(
             $primary->getPath(),
